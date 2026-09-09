e85d4dde27392ba1203699088b6360301719d8d2
Vishwajeet Singh <vishwajeetsingh9449@gmail.com>
2026-09-09 12:47:25 +0530

    Add plugin header information to openconsent-eu.php

--- openconsent-eu.php
+++ openconsent-eu.php
@@ -1,1 +1,15 @@
+<?php
+/**
+ * Plugin Name: OpenConsent EU
+ * Plugin URI: https://github.com/YOUR-USERNAME/openconsent-eu
+ * Description: Lightweight, open-source cookie consent management for WordPress.
+ * Version: 0.1.0
+ * Author: YOUR NAME
+ * License: GPL-3.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: openconsent-eu
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
